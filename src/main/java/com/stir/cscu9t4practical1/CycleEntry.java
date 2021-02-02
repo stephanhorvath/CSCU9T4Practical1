27244ca3452522e88b1b362f0bc07b220a05f995
stephanhorvath <stephanhm@protonmail.com>
2021-02-02 14:52:24 +0000

    Moved constructor parameters to super() call in CycleEntry

--- src/main/java/com/stir/cscu9t4practical1/CycleEntry.java
+++ src/main/java/com/stir/cscu9t4practical1/CycleEntry.java
@@ -8,7 +8,7 @@ public class CycleEntry extends Entry {
     private float distance;
 
     public CycleEntry(String n, int d, int m, int y, int h, int min, int s, float dist) {
-        super();
+        super(n, d, m, y, h, min, s, dist);
         name = n;
         Calendar inst = Calendar.getInstance();
         inst.set(y, m-1, d, h, min, s);
