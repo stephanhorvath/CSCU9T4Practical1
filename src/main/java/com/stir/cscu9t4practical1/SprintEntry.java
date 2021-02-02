3c07c24d395b3a864dfee43e64e97377a4e372ed
stephanhorvath <stephanhm@protonmail.com>
2021-02-02 15:03:12 +0000

    Refactored constructor and getEntry for SprintEntry

--- src/main/java/com/stir/cscu9t4practical1/SprintEntry.java
+++ src/main/java/com/stir/cscu9t4practical1/SprintEntry.java
@@ -10,11 +10,18 @@ public class SprintEntry extends Entry {
     public SprintEntry() {};
 
     public SprintEntry(String n, int d, int m, int y, int h, int min, int s, float dist) {
-        super();
-        name = n;
-        Calendar inst = Calendar.getInstance();
-        inst.set(y,m-1,d,h,min,s);
-        dateAndTime = inst;
-        distance = dist;
+        super(n, d, m, y, h, min, s, dist);
+        // name = n;
+        // Calendar inst = Calendar.getInstance();
+        // inst.set(y,m-1,d,h,min,s);
+        // dateAndTime = inst;
+        // distance = dist;
     } //constructor
+
+    public String getEntry () {
+        String result = getName()+" sprinted for " + getDistance() + " km in "
+                +getHour()+":"+getMin()+":"+ getSec() + " on "
+                +getDay()+"/"+getMonth()+"/"+getYear()+"\n";
+        return result;
+    } //getEntry
 }
