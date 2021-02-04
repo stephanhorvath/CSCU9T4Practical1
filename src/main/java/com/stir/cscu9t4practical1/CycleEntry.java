74ac7c6c7d239d575ec2072e7121921f1a6a83b0
stephanhorvath <stephanhm@protonmail.com>
2021-02-04 10:35:42 +0000

    Added terrain & tempo variables to CycleEntry

--- src/main/java/com/stir/cscu9t4practical1/CycleEntry.java
+++ src/main/java/com/stir/cscu9t4practical1/CycleEntry.java
@@ -3,18 +3,21 @@ package com.stir.cscu9t4practical1;
 import java.util.Calendar;
 
 public class CycleEntry extends Entry {
+    private String terrain;
+    private String tempo;
 
-    public CycleEntry(String n, int d, int m, int y, int h, int min, int s, float dist) {
+    public String getTerrain() { return terrain; }
+    public String getTempo() { return tempo; }
+
+    public CycleEntry(String n, int d, int m, int y, int h, int min, int s, float dist, String trrn, String t) {
         super(n, d, m, y, h, min, s, dist);
-        // name = n;
-        // Calendar inst = Calendar.getInstance();
-        // inst.set(y, m-1, d, h, min, s);
-        // dateAndTime = inst;
-        // distance = dist;
+        terrain = trrn;
+        tempo = t;
     } //constructor
 
     public String getEntry () {
-        String result = this.getName()+" cycled " + this.getDistance() + " km in "
+        String result = this.getName()+" cycled " + this.getDistance() + " km over "
+                +this.getTerrain() + " with a " + this.getTempo() + " tempo, in "
                 +this.getHour()+":"+this.getMin()+":"+ this.getSec() + " on "
                 +this.getDay()+"/"+this.getMonth()+"/"+this.getYear()+"\n";
         return result;
